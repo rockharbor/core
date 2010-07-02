5f1e07e2d04b3a9a06a2d0da218a0df712298509
Jeremy Harris <jharris@rockharbor.org>
2010-07-02 10:31:50 -0700

    Removed parentNode functionality to avoid confusion

--- app/models/group.php
+++ app/models/group.php
@@ -53,26 +53,13 @@ class Group extends AppModel {
  * Finds the parent of this group for Acl
  *
  * This function is only needed to save and edit groups, however AclBehavior
- * throws an error when it doesn't exist, so since it needs to it might as
- * well work properly just in case we decide to let someone edit groups from
- * the app.
+ * throws an error when it doesn't exist.
  *
  * @return mixed The parent, or null if none
  * @access public
  */
 	function parentNode() {
-		if (!$this->id && empty($this->data)) {
-			return null;
-		}
-		$data = $this->data;
-		if (empty($this->data)) {
-			$data = $this->read();
-		}
-		if (empty($data['Group']['parent_id'])) {
-			return null;
-		} else {
-			return $data['Group']['parent_id'];
-		}
+		
 	}
 }
 ?>
