094efce911bc3d22932b014de3ef507e651f4fb0
Jeremy Harris <jharris@rockharbor.org>
2010-07-30 08:10:04 -0700

    Updated Alerts controller test case to use CoreTestCase

--- app/tests/cases/controllers/alerts_controller.test.php
+++ app/tests/cases/controllers/alerts_controller.test.php
@@ -8,7 +8,7 @@ Mock::generate('QueueEmailComponent');
 Mock::generatePartial('AlertsController', 'TestAlertsController', array('render', 'redirect', '_stop', 'header'));
 
 class AlertsControllerTestCase extends CoreTestCase {
-	var $fixtures = array('app.ministries_rev', 'app.involvements_rev','app.alert', 'app.group', 'app.user', 'app.profile', 'app.classification', 'app.job_category', 'app.school', 'app.campus', 'plugin.media.attachment', 'app.ministry', 'app.involvement', 'app.involvement_type', 'app.address', 'app.zipcode', 'app.region', 'app.date', 'app.payment_option', 'app.question', 'app.roster', 'app.role', 'app.roster_status', 'app.answer', 'app.payment', 'app.payment_type', 'app.leader', 'app.comment', 'app.comment_type', 'app.comments', 'app.notification', 'app.image', 'plugin.media.document', 'app.household_member', 'app.household', 'app.publication', 'app.publications_user', 'app.alerts_user', 'app.log', 'app.app_setting');
+	var $fixtures = array('app.ministries_rev', 'app.involvements_rev','app.alert', 'app.group', 'app.user', 'app.profile', 'app.classification', 'app.job_category', 'app.school', 'app.campus', 'plugin.media.attachment', 'app.ministry', 'app.involvement', 'app.involvement_type', 'app.address', 'app.zipcode', 'app.region', 'app.date', 'app.payment_option', 'app.question', 'app.roster', 'app.role', 'app.roster_status', 'app.answer', 'app.payment', 'app.payment_type', 'app.leader', 'app.comment', 'app.comment_type', 'app.comments', 'app.notification', 'app.image', 'plugin.media.document', 'app.household_member', 'app.household', 'app.publication', 'app.publications_user', 'app.alerts_user', 'app.log', 'app.app_setting', 'app.aco', 'app.aro', 'app.aros_aco');
 
 /**
  * Disable inserting all records by default. Use CakeTestCase::loadFixtures
@@ -17,6 +17,7 @@ class AlertsControllerTestCase extends CoreTestCase {
 	var $autoFixtures = false;
 
 	function startTest() {
+		$this->loadFixtures('Aco', 'Aro', 'ArosAco');
 		$this->loadFixtures('Alert', 'Group', 'AlertsUser', 'User');
 		$this->Alerts =& new TestAlertsController();
 		$this->Alerts->constructClasses();
