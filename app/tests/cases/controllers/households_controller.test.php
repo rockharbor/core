0a727b67a975775dd21b2fb3023008dc3f99f4a9
Jeremy Harris <jharris@rockharbor.org>
2010-07-30 08:31:14 -0700

    Updated Households controller to use CoreTestCase

--- app/tests/cases/controllers/households_controller.test.php
+++ app/tests/cases/controllers/households_controller.test.php
@@ -1,18 +1,13 @@
 <?php
 /* Households Test cases generated on: 2010-07-12 10:07:39 : 1278957279 */
+App::import('Lib', 'CoreTestCase');
+App::import('Component', 'QueueEmail');
 App::import('Controller', 'Households');
 
-class TestHouseholdsController extends HouseholdsController {
-	function redirect($url, $status = null, $exit = true) {
-		$this->redirectUrl = $url;
-	}
+Mock::generate('QueueEmailComponent');
+Mock::generatePartial('HouseholdsController', 'TestHouseholdsController', array('isAuthorized', 'render', 'redirect', '_stop', 'header'));
 
-	function _stop($status = 0) {
-		$this->stopped = $status;
-	}
-}
-
-class HouseholdsControllerTestCase extends CakeTestCase {
+class HouseholdsControllerTestCase extends CoreTestCase {
 	var $fixtures = array('app.ministries_rev', 'app.involvements_rev','app.notification', 'app.user', 'app.group', 'app.profile', 'app.classification', 'app.job_category', 'app.school', 'app.campus', 'plugin.media.attachment', 'app.ministry', 'app.involvement', 'app.involvement_type', 'app.address', 'app.zipcode', 'app.region', 'app.date', 'app.payment_option', 'app.question', 'app.roster', 'app.role', 'app.roster_status', 'app.answer', 'app.payment', 'app.payment_type', 'app.leader', 'app.comment', 'app.comment_type', 'app.comments', 'app.notification', 'app.image', 'plugin.media.document', 'app.household_member', 'app.household', 'app.publication', 'app.publications_user', 'app.log', 'app.app_setting', 'app.alert', 'app.alerts_user', 'app.log');
 
 /**
@@ -26,8 +21,10 @@ class HouseholdsControllerTestCase extends CakeTestCase {
 		$this->Households =& new TestHouseholdsController();
 		$this->Households->constructClasses();
 		$this->Households->Component->initialize($this->Households);
-		$this->Households->Session->write('Auth.User', array('id' => 1));
-		$this->Households->Session->write('User', array('Group' => array('id' => 1)));
+		$this->Households->QueueEmail = new MockQueueEmailComponent();
+		$this->Households->setReturnValue('isAuthorized', true);
+		$this->Households->QueueEmail->setReturnValue('send', true);
+		$this->testController = $this->Households;
 	}
 
 	function endTest() {
@@ -37,7 +34,7 @@ class HouseholdsControllerTestCase extends CakeTestCase {
 	}
 
 	function testShiftHousehold() {
-		$this->testAction('/test_households/shift_households/1/1');
+		$this->testAction('/households/shift_households/1/1');
 		$householdMember = $this->Households->Household->HouseholdMember->find('all', array(
 			'conditions' => array(
 				'HouseholdMember.user_id' => 1
@@ -46,7 +43,7 @@ class HouseholdsControllerTestCase extends CakeTestCase {
 		$this->assertEqual($householdMember[0]['HouseholdMember']['id'], 4);
 		$this->assertEqual($householdMember[0]['HouseholdMember']['household_id'], 1);
 
-		$this->testAction('/test_households/shift_households/1/2');
+		$this->testAction('/households/shift_households/1/2');
 		$householdMember = $this->Households->Household->HouseholdMember->find('all', array(
 			'conditions' => array(
 				'HouseholdMember.user_id' => 1
@@ -63,7 +60,7 @@ class HouseholdsControllerTestCase extends CakeTestCase {
 			'household_id' => 2,
 			'confirmed' => true
 		));
-		$this->testAction('/test_households/make_household_contact/1/2');
+		$this->testAction('/households/make_household_contact/1/2');
 		$results = $this->Households->Household->find('all', array(
 			'fields' => array('id', 'contact_id')
 		));
@@ -85,7 +82,7 @@ class HouseholdsControllerTestCase extends CakeTestCase {
 	}
 
 	function testIndex() {
-		$vars = $this->testAction('/test_households/index/User:1', array(
+		$vars = $this->testAction('/households/index/User:1', array(
 			'return' => 'vars'
 		));
 		$results = Set::extract('/Household/id', $vars['households']);
@@ -97,7 +94,7 @@ class HouseholdsControllerTestCase extends CakeTestCase {
 			'household_id' => 2,
 			'confirmed' => true
 		));
-		$vars = $this->testAction('/test_households/index/User:1', array(
+		$vars = $this->testAction('/households/index/User:1', array(
 			'return' => 'vars'
 		));
 		$results = Set::extract('/Household/id', $vars['households']);
