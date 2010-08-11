c538b4dd8fdc6fe6b291f830e023c964c2bdfedd
Jeremy Harris <jharris@rockharbor.org>
2010-08-11 14:03:40 -0700

    Added tests for Profile model and added to group. Refs #25

---
+++ app/tests/cases/models/profile.test.php
@@ -0,0 +1,67 @@
+<?php
+/* Roster Test cases generated on: 2010-07-26 14:07:11 : 1280180951 */
+App::import('Lib', 'CoreTestCase');
+App::import('Model', 'Profile');
+
+class ProfileTestCase extends CoreTestCase {
+
+	function startTest() {
+		$this->loadFixtures('Profile');
+		$this->Profile =& ClassRegistry::init('Profile');
+	}
+
+	function endTest() {
+		unset($this->Profile);
+		ClassRegistry::flush();
+	}
+
+	function testVirtualFields() {
+		$profile = $this->Profile->read(null, 1);
+		$profile['Profile']['birth_date'] = date('Y-m-d', strtotime('-20 years'));
+		$this->Profile->save($profile);
+		$profile = $this->Profile->read(null, 1);
+
+		$result = $profile['Profile']['age'];
+		$this->assertWithinMargin($result, 20, 1);
+
+		$result = $profile['Profile']['name'];
+		$this->assertEqual($result, 'Jeremy Harris');
+
+		$result = $profile['Profile']['child'];
+		$this->assertFalse($result, 0);
+
+		$profile = $this->Profile->read(null, 1);
+		$profile['Profile']['birth_date'] = date('Y-m-d', strtotime('-12 years'));
+		$this->Profile->save($profile);
+		$profile = $this->Profile->read(null, 1);
+
+		$result = $profile['Profile']['age'];
+		$this->assertWithinMargin($result, 12, 1);
+
+		$result = $profile['Profile']['child'];
+		$this->assertFalse($result, 0);
+
+		$profile = $this->Profile->read(null, 1);
+		$profile['Profile']['birth_date'] = date('Y-m-d', strtotime('-12 years'));
+		$profile['Profile']['adult'] = false;
+		$this->Profile->save($profile);
+		$profile = $this->Profile->read(null, 1);
+
+		$result = $profile['Profile']['age'];
+		$this->assertWithinMargin($result, 12, 1);
+
+		$result = $profile['Profile']['child'];
+		$this->assertTrue($result, 1);
+
+		$profile = $this->Profile->read(null, 1);
+		$profile['Profile']['birth_date'] = null;
+		$profile['Profile']['adult'] = false;
+		$this->Profile->save($profile);
+		$profile = $this->Profile->read(null, 1);
+
+		$result = $profile['Profile']['child'];
+		$this->assertTrue($result, 1);
+	}
+
+}
+?>
--- app/tests/groups/models.group.php
+++ app/tests/groups/models.group.php
@@ -14,6 +14,7 @@ class ModelsGroupTest extends TestSuite {
 		TestManager::addTestFile($this, APP . 'tests' . DS . 'cases' . DS . 'models' . DS . 'involvement');
 		TestManager::addTestFile($this, APP . 'tests' . DS . 'cases' . DS . 'models' . DS . 'leader');
 		TestManager::addTestFile($this, APP . 'tests' . DS . 'cases' . DS . 'models' . DS . 'ministry');
+		TestManager::addTestFile($this, APP . 'tests' . DS . 'cases' . DS . 'models' . DS . 'profile');
 		TestManager::addTestFile($this, APP . 'tests' . DS . 'cases' . DS . 'models' . DS . 'roster');
 		TestManager::addTestFile($this, APP . 'tests' . DS . 'cases' . DS . 'models' . DS . 'school');
 		TestManager::addTestFile($this, APP . 'tests' . DS . 'cases' . DS . 'models' . DS . 'sys_email');
