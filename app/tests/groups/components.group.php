640d6acdc2ac2fe5d7b62c119150b43cd4a9c0ec
Jeremy Harris <jharris@rockharbor.org>
2010-08-13 13:02:18 -0700

    Added AuthorizeDotNet component test to group

--- app/tests/groups/components.group.php
+++ app/tests/groups/components.group.php
@@ -5,6 +5,7 @@ class ComponentsGroupTest extends TestSuite {
 	var $label = 'Component tests';
 
 	function ComponentsGroupTest() {
+		TestManager::addTestFile($this, APP . 'tests' . DS . 'cases' . DS . 'components' . DS . 'authorize_dot_net');
 		TestManager::addTestFile($this, APP . 'tests' . DS . 'cases' . DS . 'components' . DS . 'notifier');
 		TestManager::addTestFile($this, APP . 'tests' . DS . 'cases' . DS . 'components' . DS . 'filter_pagination');
 	}
