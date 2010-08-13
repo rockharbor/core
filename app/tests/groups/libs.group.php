44e04a838e373473bc8c46aa04adc59b4e8b569a
Jeremy Harris <jharris@rockharbor.org>
2010-08-13 13:02:25 -0700

    Created libs group

---
+++ app/tests/groups/libs.group.php
@@ -0,0 +1,12 @@
+<?php
+
+class LibsGroupTest extends TestSuite {
+
+	var $label = 'Lib tests';
+
+	function LibsGroupTest() {
+		TestManager::addTestFile($this, APP . 'tests' . DS . 'cases' . DS . 'libs' . DS . 'core');
+		TestManager::addTestFile($this, APP . 'tests' . DS . 'cases' . DS . 'libs' . DS . 'core_test_case');
+		TestManager::addTestFile($this, APP . 'tests' . DS . 'cases' . DS . 'libs' . DS . 'email_listener');
+	}
+}
