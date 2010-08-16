7c39965b6cc43134e9ce49b9c1966815c9dbfaf3
Jeremy Harris <jharris@rockharbor.org>
2010-08-16 08:54:24 -0700

    Added migration for involvement linking in ministries

--- app/config/migrations/map.php
+++ app/config/migrations/map.php
@@ -2,5 +2,7 @@
 $map = array(
 	1 => array(
 		'initial_migration' => 'M4c695df4aba043babe0d0fdcb36fd16a'),
+	2 => array(
+		'added_involvement_links' => 'M4c695e1dc65c4cb596470fccb36fd16a'),
 );
 ?>
