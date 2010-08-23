e21ecc7cc62e60668f7b8981812fbc8911e5115c
Jeremy Harris <jharris@rockharbor.org>
2010-08-23 15:02:20 -0700

    Formatted code

--- app/libs/listeners/screen.php
+++ app/libs/listeners/screen.php
@@ -1,23 +1,25 @@
 <?php
+/**
+ * ScreenListener
+ * 
+ * Restores error handling in the browser when debug is enabled.
+ *
+ * @author Frank de Graaf (Phally)
+ */
+class ScreenListener {
 
-	/**
-	 * ScreenListener
-	 * Restores error handling in the browser when debug is enabled.
-	 * @author Frank de Graaf (Phally)
-	 */
-	class ScreenListener {
-		
-		/**
-		 * Triggered when we're passed an error from the WhistleComponent
-		 * @param array $error
-		 * @param array $configuration
-		 * @return null
-		 * @access public
-		 */
-		public function error($error, $configuration = array()) {
-			if (Configure::read() > 0) {
-				call_user_func_array(array('Debugger', 'handleError'), array_values($error));
-			}
+/**
+ * Triggered when we're passed an error from the WhistleComponent
+ *
+ * @param array $error
+ * @param array $configuration
+ * @return null
+ * @access public
+ */
+	public function error($error, $configuration = array()) {
+		if (Configure::read() > 0) {
+			call_user_func_array(array('Debugger', 'handleError'), array_values($error));
 		}
 	}
+}
 ?>
