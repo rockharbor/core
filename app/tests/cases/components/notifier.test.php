59349699fa6545e6cb02d6f7425052d5475e4505
Jeremy Harris <jharris@rockharbor.org>
2010-09-30 11:20:42 -0700

    Centralizing notification code

--- app/tests/cases/components/notifier.test.php
+++ app/tests/cases/components/notifier.test.php
@@ -1,7 +1,9 @@
 <?php
 App::import('Lib', 'CoreTestCase');
 App::import('Model', 'Notification');
-App::import('Component', 'Notifier');
+App::import('Component', array('Notifier', 'QueueEmail.QueueEmail'));
+
+Mock::generatePartial('QueueEmailComponent', 'MockQueueEmailComponent', array('send'));
 
 class TestNotifierController extends Controller {}
 
@@ -13,7 +15,9 @@ class NotifierTestCase extends CoreTestCase {
 		$this->Controller = new TestNotifierController();
 		$this->Notifier->initialize($this->Controller, array());
 		$this->Notification = ClassRegistry::init('Notification');
-		$this->Notifier->notification = $this->Notification;
+		$this->Notification->QueueEmail = new MockQueueEmailComponent();
+		$this->Notification->QueueEmail->initialize($this->Controller);
+		$this->Notification->QueueEmail->setReturnValue('send', true);
 	}
 
 	function endTest() {
@@ -33,7 +37,11 @@ class NotifierTestCase extends CoreTestCase {
 		$this->assertFalse($this->Notifier->notify(1, 'nonexistantnotification'));
 	}
 
-	function test_save() {
+	function testSend() {
+
+	}
+
+	function testSave() {
 		$dateReg = 'preg:/([0-9]{2,4})-([0-1][0-9])-([0-3][0-9]) (?:([0-2][0-9]):([0-5][0-9]):([0-5][0-9]))?/';
 
 		$data = array(
@@ -76,7 +84,7 @@ class NotifierTestCase extends CoreTestCase {
 		$this->assertEqual($result, $expected);
 	}
 
-	function test_render() {
+	function testRender() {
 		$this->Controller->set('name', 'Jeremy');
 		$this->Controller->set('type', 'leading');
 		$this->Controller->set('itemType', 'Team');
