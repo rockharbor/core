aab87e8b63186a695e211b3b86b0c9bdffc7501f
Jeremy Harris <jharris@rockharbor.org>
2010-12-07 07:41:12 -0800

    Add entered_by after all users are migrated

--- app/plugins/migrator/vendors/shells/tasks/cleanup.php
+++ app/plugins/migrator/vendors/shells/tasks/cleanup.php
@@ -13,6 +13,31 @@ class CleanupTask extends MigratorShell {
 		$this->Ministry = ClassRegistry::init('Ministry');
 		$this->Ministry->Behaviors->attach('Tree');
 		$this->Ministry->recover();
+
+		$this->out('Setting entered by id');
+		$this->User = ClassRegistry::init('User');
+		$data = $this->User->find('all', array(
+			'contain' => array(
+				'Profile' => array(
+					'fields' => array('id', 'user_id', 'created_by')
+				)
+			)
+		));
+		foreach($data as $user) {
+			$link = $this->IdLinkage->find('first', array(
+				'conditions' => array(
+					'new_model' => 'User',
+					'old_table' => 'person',
+					'old_pk' => $user['Profile']['created_by']
+				)
+			));
+			if ($link === false || empty($link)) {
+				CakeLog::write('migration', 'During cleanup, unable to find person # '.$user['Profile']['created_by']);
+				$link['IdLinkage']['new_pk'] = '';
+			}
+			$this->User->Profile->id = $user['Profile']['id'];
+			$this->User->Profile->saveField('created_by', $link['IdLinkage']['new_pk']);
+		}
 	}
 
 
