60eb428ff93287301d787749f9d1fee9f9325896
Jeremy Harris <jharris@rockharbor.org>
2010-12-14 08:55:03 -0800

    Fixed bug where swapping tabs reset equal-height boxes in other tabs

--- app/webroot/js/global.js
+++ app/webroot/js/global.js
@@ -134,7 +134,7 @@ CORE.init = function() {
  * opens
  */
 CORE.initUI = function() {
-	$('.equal-height .content-box').equalHeights();
+	$('.equal-height:visible .content-box').equalHeights();
 	// form elements
 	CORE.initFormUI();
 	// hide flash message
