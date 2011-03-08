c5ff2e83f39adbc90a5d922e884987151384dd87
Jeremy Harris <jharris@rockharbor.org>
2011-03-08 08:40:10 -0800

    Fixed bug where complex form fields were thought to be HABTM

---
+++ app/app_helper.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * App helper class.
+ *
+ * @copyright     Copyright 2010, *ROCK*HARBOR
+ * @link          http://rockharbor.org *ROCK*HARBOR
+ * @package       core
+ * @subpackage    core.app
+ */
+
+/**
+ * App Helper
+ *
+ * @package       core
+ * @subpackage    core.app
+ */
+class AppHelper extends Helper {
+
+/**
+ * Transforms a recordset from a hasAndBelongsToMany association to a list of selected
+ * options for a multiple select element
+ *
+ * Overridden to allow deep form relations, like `HouseholdMember.0.Profile.first_name`
+ * without errors because it is assumed to be a HABTM select field
+ *
+ * @param mixed $data
+ * @param string $key
+ * @return array
+ * @access private
+ */
+	function __selectedArray($data, $key = 'id') {
+		if (!is_array($data)) {
+			$model = $data;
+			if (!empty($this->data[$model][$model])) {
+				return $this->data[$model][$model];
+			}
+			if (!empty($this->data[$model])) {
+				$data = $this->data[$model];
+			}
+		}
+		$array = array();
+		if (!empty($data)) {
+			foreach ($data as $var) {
+				if (isset($var[$key])) {
+					$array[$var[$key]] = $var[$key];
+				}
+			}
+		}
+		return $array;
+	}
+}
+?>
---
+++ app/tests/cases/helpers/app_helper.test.php
@@ -0,0 +1,41 @@
+<?php
+App::import('Lib', 'CoreTestCase');
+App::import('Helper', array('App'));
+
+/**
+ * Proxy class to allow access to protected methods
+ */
+class ProxyAppHelper extends AppHelper {
+	function selectedArray($data, $key = 'id') {
+		$this->__selectedArray($data, $key);
+	}
+}
+
+class FormattingHelperTestCase extends CoreTestCase {
+
+	function startTest() {
+		$this->App = new ProxyAppHelper();
+	}
+
+	function endTest() {
+		unset($this->App);
+		ClassRegistry::flush();
+	}
+
+	function testSelectedArray() {
+		$data = array(
+			'Some' => array(
+				'Embedded' => array(
+					'model',
+					'data',
+					'that',
+					'is',
+					'not',
+					'habtm'
+				)
+			)
+		);
+		$this->assertEqual($this->App->selectedArray($data), array());
+	}
+
+}
