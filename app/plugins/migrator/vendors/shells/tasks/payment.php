e85104c9cd6b12cb05a3bb3f3f590f1ebc513a3c
Jeremy Harris <jharris@rockharbor.org>
2011-05-03 10:49:05 -0700

    Fixed payment migration to account for credit card payments already existing on a roster record. Added mapping for new PaymentType structure.

--- app/plugins/migrator/vendors/shells/tasks/payment.php
+++ app/plugins/migrator/vendors/shells/tasks/payment.php
@@ -12,6 +12,14 @@ class PaymentTask extends MigratorTask {
 	var $_oldTable = 'credit_card_payment';
 	var $_oldPk = 'credit_card_payment_id';
 	var $_newModel = 'Payment';
+	
+	var $_creditCardTypeMap = array(
+		null => 1,
+		0 => 1,
+		'VISA' => 1,
+		'MASTERCARD' => 2,
+		'AMEX' => 3
+	);
 
 	function mapData() {
 		$this->_editingRecord = array(
@@ -19,7 +27,7 @@ class PaymentTask extends MigratorTask {
 				'user_id' => $this->_editingRecord['person_id'],
 				'roster_id' => $this->_editingRecord['event_roster_id'],
 				'amount' => $this->_editingRecord['payment_amount'],
-				'payment_type_id' => 1, //credit card
+				'payment_type_id' => $this->_editingRecord['credit_card_type'],
 				'number' => $this->_editingRecord['cc_number'],
 				'transaction_id' => $this->_editingRecord['transaction_id'],
 				'payment_placed_by' => $this->_editingRecord['payment_placed_by'],
--- app/plugins/migrator/vendors/shells/tasks/payment_roster.php
+++ app/plugins/migrator/vendors/shells/tasks/payment_roster.php
@@ -18,13 +18,68 @@ class PaymentRosterTask extends MigratorTask {
 		$options = array(
 			'order' => $this->_oldPk,
 			'conditions' => array(
-				'paymentComments !=' => ''
+				'Model.amountPaid >' => 0
 			)
 		);
+		
+		// needed for bindmodel
+		$this->old->primaryKey = $this->_oldPk;
+		
+		// save this model in class registry since table name/keys are wonky
+		new Model(array(
+			'table' => 'credit_card_payment',
+			'ds' => $this->_oldDbConfig,
+			'name' => 'CreditCardPayment'
+		));
+		// bind credit card model
+		$this->old->bindModel(array(
+			'hasMany' => array(
+				'CreditCardPayment' => array(
+					'className' => 'CreditCardPayment',
+					'table' => 'credit_card_payment',
+					'foreignKey' => 'event_roster_id'
+				)
+			)
+		));
+		
 		if ($limit) {
 			$options['limit'] = $limit;
 		}
+		$this->old->recursive = 1;
 		return $this->old->find('all', $options);
+	}
+	
+/**
+ * Checks to see if this roster has a credit card payment associated with it. If not,
+ * it will add a payment if there is an amount paid
+ * 
+ * @param string $amount 
+ */
+	function _prepareAmountPaid($amount) {
+		// add up all credit card payments
+		$sum = (int)Set::apply('/CreditCardPayment/payment_amount', $this->_originalRecord, 'array_sum');
+		// subtract from amount paid
+		$amt = $this->_editingRecord['amountPaid'] - $sum;
+		if ($amt == 0) {
+			$this->_editingRecord = false;
+			return false;
+		} else {
+			$this->out('Creating a cash payment for $'.$amt);
+		}
+		return $amt;
+	}
+	
+/**
+ * Sanitizes comments
+ * 
+ * @param string $old
+ * @return string
+ */
+	function _preparePaymentComments($old) {
+		$old = Sanitize::html($old, array(
+			'remove' => true,
+		));
+		return Sanitize::html(nl2br($old));
 	}
 
 	function mapData() {
@@ -33,7 +88,7 @@ class PaymentRosterTask extends MigratorTask {
 				'user_id' => $this->_editingRecord['person_id'],
 				'roster_id' => $this->_editingRecord['event_roster_id'],
 				'amount' => $this->_editingRecord['amountPaid'],
-				'payment_type_id' => null, // unknown
+				'payment_type_id' => 4, // consider it a cash transaction
 				'number' => null,
 				'transaction_id' => null,
 				'payment_placed_by' => null,
