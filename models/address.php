rockharbor/core

commit 8e49aedb52d9ee27e257586e965329a9ce09178c

Many content changes

models/address.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
 /**
  * Address model class.
  *
@@ -56,7 +56,7 @@ class Address extends AppModel {
 			'rule' => array('custom', '/^[a-z0-9 ]*$/i'),
 			'required' => false,
 			'allowEmpty' => true,
-			'message' => 'Alpha-numeric characters only.'
+			'message' => 'Please use alpha and numeric characters only.'
 		),
 		'zip' => array(
 			'rule' => array('postal', null, 'us'),

commit f9225f280734ab64fe7eeb4999681e5a309a3b6c

Fixed whitespace bug introduced in 8e49aed

models/address.php
@@ -1,4 +1,4 @@
- <?php
+<?php
 /**
  * Address model class.
  *