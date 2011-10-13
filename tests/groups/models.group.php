9f1bbf2df4724e0ec9fcb09f71ace50aa1c6523d
Jeremy Harris <jharris@rockharbor.org>
2011-10-13 12:04:35 -0700

    Added missing tests in models group

--- tests/groups/models.group.php
+++ tests/groups/models.group.php
@@ -5,12 +5,16 @@ class ModelsGroupTest extends TestSuite {
 	var $label = 'Model tests';
 
 	function ModelsGroupTest() {
+		TestManager::addTestFile($this, APP . 'tests' . DS . 'cases' . DS . 'models' . DS . 'address');
 		TestManager::addTestFile($this, APP . 'tests' . DS . 'cases' . DS . 'models' . DS . 'alert');
 		TestManager::addTestFile($this, APP . 'tests' . DS . 'cases' . DS . 'models' . DS . 'app_model');
 		TestManager::addTestFile($this, APP . 'tests' . DS . 'cases' . DS . 'models' . DS . 'campus');
+		TestManager::addTestFile($this, APP . 'tests' . DS . 'cases' . DS . 'models' . DS . 'comment');
 		TestManager::addTestFile($this, APP . 'tests' . DS . 'cases' . DS . 'models' . DS . 'date');
 		TestManager::addTestFile($this, APP . 'tests' . DS . 'cases' . DS . 'models' . DS . 'group');
 		TestManager::addTestFile($this, APP . 'tests' . DS . 'cases' . DS . 'models' . DS . 'household');
+		TestManager::addTestFile($this, APP . 'tests' . DS . 'cases' . DS . 'models' . DS . 'image');
+		TestManager::addTestFile($this, APP . 'tests' . DS . 'cases' . DS . 'models' . DS . 'invitation');
 		TestManager::addTestFile($this, APP . 'tests' . DS . 'cases' . DS . 'models' . DS . 'involvement');
 		TestManager::addTestFile($this, APP . 'tests' . DS . 'cases' . DS . 'models' . DS . 'leader');
 		TestManager::addTestFile($this, APP . 'tests' . DS . 'cases' . DS . 'models' . DS . 'ministry');
