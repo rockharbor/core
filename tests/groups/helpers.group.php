26dd4f77d9d6e939af57bf966bd36d7c64cba61c
Jeremy Harris <jharris@rockharbor.org>
2011-10-13 11:30:29 -0700

    Added missing test into helpers group

--- tests/groups/helpers.group.php
+++ tests/groups/helpers.group.php
@@ -5,6 +5,7 @@ class HelpersGroupTest extends TestSuite {
 	var $label = 'Helper tests';
 
 	function HelpersGroupTest() {
+		TestManager::addTestFile($this, APP . 'tests' . DS . 'cases' . DS . 'helpers' . DS . 'app_helper');
 		TestManager::addTestFile($this, APP . 'tests' . DS . 'cases' . DS . 'helpers' . DS . 'formatting');
 		TestManager::addTestFile($this, APP . 'tests' . DS . 'cases' . DS . 'helpers' . DS . 'permission');
 		TestManager::addTestFile($this, APP . 'tests' . DS . 'cases' . DS . 'helpers' . DS . 'select_options');
