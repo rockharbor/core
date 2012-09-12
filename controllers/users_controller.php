fd27c39da119d1af2582e2a675ed678bb0ba63c9
Jeremy Harris <jharris@rockharbor.org>
2012-09-12 10:28:58 -0700

    Added clarity to merge request flash message

--- controllers/users_controller.php
+++ controllers/users_controller.php
@@ -348,7 +348,7 @@ class UsersController extends AppController {
 					'template' => 'users_request_activation',
 					'subject' => 'Account merge request',
 				));
-				$this->Session->setFlash('Account merge request has been received.', 'flash'.DS.'success');
+				$this->Session->setFlash('Account merge request has been received. You will receive an email when it is confirmed!', 'flash'.DS.'success');
 			} else {
 				$this->Session->setFlash('Unable to send account merge request. Please try again.', 'flash'.DS.'failure');
 			}			
